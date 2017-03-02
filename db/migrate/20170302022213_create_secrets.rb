eabaa29d23c258916ba1f820acd38ff3093118f4
Matt Desmet <mattd.email@gmail.com>
2017-03-01 18:22:34 -0800

    added secret scaffold

---
+++ db/migrate/20170302022213_create_secrets.rb
@@ -0,0 +1,10 @@
+class CreateSecrets < ActiveRecord::Migration[5.0]
+  def change
+    create_table :secrets do |t|
+      t.string :title
+      t.text :content
+
+      t.timestamps
+    end
+  end
+end
